e334548358026557bbb45aeba86966b405c44727
Zh4ka <evgenijsturmak@gmail.com>
2021-09-09 09:52:46 +0300

    seve all file

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/Hoby')
+def hoby():
+    return render_template("Hoby.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
